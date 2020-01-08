d0058fa386d6db659ae18d5cc40a12c6c268a2c0
Nitin Bhat <nbhat4@illinois.edu>
2020-01-08 13:21:11 -0600

    Add skeleton for scripts to help create tar and evaluate output

---
+++ scripts/createExerciseTar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# Create a tar file from only the required files/folders for the students to work on
---
+++ scripts/evaluateOutput.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# Input 1 will be a tar file that contains 1 main output file and a lot of cell output files
+# Input 2 will be the path to a folder that contains 1 main output file and a lot of cell output files
+
+
+# Untar the input file into a folder
+
+# check that the inputs are correct
+
+# move the main output file out of the folder
+
+# Extract the value for time taken, min and max
+
+# check correctness of final cell values by diffing cell output from files of folder 1 and folder 2
+
