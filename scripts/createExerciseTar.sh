9b1ba9b2fa757511b04530b806a83a63ad158882
Nitin Bhat <nbhat4@illinois.edu>
2020-01-09 14:23:16 -0600

    Add scripts for creating an exercise tar and evaluating result output

--- scripts/createExerciseTar.sh
+++ scripts/createExerciseTar.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+#=====================================================================================
+# Script creates a tar file folderName.tar.gz which will be passed on to the students
+#=====================================================================================
 
-# Create a tar file from only the required files/folders for the students to work on
+#Set particleSimDir to the home directory of particleSimulation
+particleSimDir="/Users/nitinbhat/Work/software/particleSimulation"
+folderName="charm-exercise"
+
+cd $particleSimDir
+pwd
+make clean
+mkdir $folderName
+
+cp ./README.md ./$folderName
+cp ./Makefile ./$folderName
+cp -r ./img ./$folderName
+cp -r ./src ./$folderName
+mkdir $folderName/obj $folderName/output
+
+tar -czvf $folderName.tar.gz ./$folderName
+rm -rf ./$folderName
--- scripts/evaluateOutput.sh
+++ scripts/evaluateOutput.sh
@@ -1,17 +1,110 @@
 #!/bin/bash
+#=====================================================================================
+# Script evaluates the tar file submitted by the students to check for correctness
+# and determine the total time taken by the application
+#=====================================================================================
 
+#Set compareDir to the directory that contains the verified output of the application
+#The output submitted by the students will be compared against this output
+compareDir="/Users/nitinbhat/Work/software/particleSimulation/scripts/compareOutput/"
+inputTar=$1
 
-# Input 1 will be a tar file that contains 1 main output file and a lot of cell output files
-# Input 2 will be the path to a folder that contains 1 main output file and a lot of cell output files
+if [[ -z "$inputTar" ]]; then
+  echo "Tar file value not passed in the first parameter! Usage: ./evaluateOutput.sh <path-to-tar-file>";
+  exit 1
+fi
 
+if [[ ! -f "$inputTar" ]]; then
+  echo "Tar file $inputTar does not exist!"
+  exit 1
+fi
 
-# Untar the input file into a folder
+tar -xvf $inputTar
+inputDir=`ls -td -- */ | head -n 1`
 
-# check that the inputs are correct
+declare -a compareOutput
+declare -a compareInput
+declare -a evalOutput
+declare -a evalInput
+declare -a inputArr
+declare -a outputArr
 
-# move the main output file out of the folder
+function parse_main_output {
+  file=$1;
+  while IFS= read -r line
+  do
+    if [[ $line =~ ^Input:Grid.*:([0-9]+) ]]; then
+      inputArr+=(${BASH_REMATCH[1]})
+    elif [[ $line =~ ^Input:Particles.*:([0-9]+) ]]; then
+      inputArr+=(${BASH_REMATCH[1]})
+    elif [[ $line =~ ^Input:Number.*:([0-9]+) ]]; then
+      inputArr+=(${BASH_REMATCH[1]})
+    elif [[ $line =~ ^Output:Total.*:([0-9]+\.[0-9]+) ]]; then
+      outputArr+=(${BASH_REMATCH[1]})
+    elif [[ $line =~ ^Output:Time.*:([0-9]+\.[0-9]+) ]]; then
+      outputArr+=(${BASH_REMATCH[1]})
+    elif [[ $line =~ ^Output:Min.*:([0-9]+) ]]; then
+      outputArr+=(${BASH_REMATCH[1]})
+    elif [[ $line =~ ^Output:Max.*:([0-9]+) ]]; then
+      outputArr+=(${BASH_REMATCH[1]})
+    fi
+  done <"$file"
+}
 
-# Extract the value for time taken, min and max
+mv $inputDir/sim_output_main ./sim_output_main_input
+mv $compareDir/sim_output_main ./sim_output_main_compare
 
-# check correctness of final cell values by diffing cell output from files of folder 1 and folder 2
+diffResult=`diff -rq $inputDir $compareDir`
 
+if [[ -z "$diffResult" ]]; then
+  echo "Particle Output is correct!"
+else
+  echo "Particle Output is incorrect!"
+fi
+
+parse_main_output ./sim_output_main_compare
+compareOutput=("${outputArr[@]}")
+compareInput=("${inputArr[@]}")
+unset inputArr
+unset outputArr
+
+parse_main_output ./sim_output_main_input
+evalOutput=("${outputArr[@]}")
+evalInput=("${inputArr[@]}")
+
+lastIndex="$((${#evalInput[@]}-1))"
+
+#check correctness of inputs
+for i in $(seq 0 $lastIndex);
+  do
+    if [ ${compareInput[$i]} -ne ${evalInput[$i]} ]; then
+      echo "Inputs are not same! Eval:${evalInput[$i]} Compare:${compareInput[$i]} ";
+      exit 1
+    fi
+done
+
+if [ ${evalOutput[2]} -eq -1 ]; then
+  echo "Minimum Bonus Question not answered!";
+else
+  if [ ${evalOutput[2]} -ne ${compareOutput[2]} ]; then
+    echo "Minimum Bonus Question answered but incorrect!";
+  else
+    echo "Minimum Bonus Question answered and correct!";
+  fi
+fi
+
+if [ ${evalOutput[3]} -eq -1 ]; then
+  echo "Maximum Bonus Question not answered!";
+else
+  if [ ${evalOutput[3]} -ne ${compareOutput[3]} ]; then
+    echo "Maximum Bonus Question answered but incorrect!";
+  else
+    echo "Maximum Bonus Question answered and correct!";
+  fi
+fi
+
+echo "Total Application Time:${evalOutput[0]}";
+echo "Total Application Time Per Step:${evalOutput[1]}";
+
+mv ./sim_output_main_compare $compareDir/sim_output_main
+mv ./sim_output_main_input $inputDir/sim_output_main
